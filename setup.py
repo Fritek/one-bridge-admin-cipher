ff04f864eaf6ba3e3d0bbe4df38aaa250a3221d1
Ayobami-00 <festusowumi@gmail.com>
2021-08-03 05:44:09 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 import versioneer
 
-with open("README.rst", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 with open("requirements.txt", "r") as fh:
     requirements = [line.strip() for line in fh]
